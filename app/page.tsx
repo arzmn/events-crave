7d7eafd09457f58dee37d3ba730d2b45252bcc0d
Somashekar B R <sanjayrao161@gmail.com>
2024-09-10 14:36:19 +0530

    test

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import AppAppBar from "@/components/Appbar/Appbar";
+import Hero from "@/components/Hero/Hero";
+
+export default function Home() {
+  return (
+    <div className="">
+      <AppAppBar />
+      <Hero />
+    </div>
+  );
+}
