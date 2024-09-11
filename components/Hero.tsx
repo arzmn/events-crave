d005eee478cfac9c744dfdc0db234ddb403a95aa
Somashekar B R <sanjayrao161@gmail.com>
2024-09-11 21:22:38 +0530

    merged from Arz-FE

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,36 +8,12 @@ import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 
 import { visuallyHidden } from "@mui/utils";
 import { styled } from "@mui/material/styles";
-import { Divider, InputAdornment } from "@mui/material";
+import { InputAdornment } from "@mui/material";
 
-const StyledBox = styled("div")(({ theme }) => ({
-  alignSelf: "center",
-  width: "100%",
-  height: 400,
-  marginTop: theme.spacing(8),
-  borderRadius: theme.shape.borderRadius,
-  outline: "6px solid",
-  outlineColor: "hsla(220, 25%, 80%, 0.2)",
-  border: "1px solid",
-  borderColor: theme.palette.grey[200],
-  boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
-  backgroundImage: `url(https://loremflickr.com/1920/1080?random=1)`,
-  backgroundSize: "cover",
-  [theme.breakpoints.up("sm")]: {
-    marginTop: theme.spacing(10),
-    height: 700,
-  },
-  ...theme.applyStyles("dark", {
-    boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
-    backgroundImage: `url(https://loremflickr.com/1920/1080?random=1)`,
-    outlineColor: "hsla(220, 20%, 42%, 0.1)",
-    borderColor: theme.palette.grey[700],
-  }),
-}));
 const PrimarySearchInput = styled(TextField)(({ theme }) => ({
   "& .MuiInputBase-root": {
     borderRadius: 26,
@@ -103,26 +79,24 @@ export default function Hero() {
             }}
           >
             Find Your Next&nbsp;
-          </Typography>  
-          
-            <Typography
-              component="span"
-              variant="h1"
-              sx={(theme) => ({
-                textAlign: "center",
+          </Typography>
+
+          <Typography
+            component="span"
+            variant="h1"
+            sx={(theme) => ({
+              textAlign: "center",
               fontSize: "clamp(3rem, 10vw, 3.5rem)",
-                // fontSize: "inherit",
-                color: "primary.main",
-                ...theme.applyStyles("dark", {
-                  color: "primary.light",
-                }),
-              })}
-            >
-              
-              Favourite Events&nbsp;
-            </Typography>
-            
-          
+              // fontSize: "inherit",
+              color: "primary.main",
+              ...theme.applyStyles("dark", {
+                color: "primary.light",
+              }),
+            })}
+          >
+            Favourite Events&nbsp;
+          </Typography>
+
           <Typography
             sx={{
               textAlign: "center",
@@ -157,16 +131,14 @@ export default function Hero() {
                         flexItem
                       /> */}
                       <Button
-                      variant="contained"
+                        variant="contained"
                         color="primary"
                         sx={{
                           minWidth: "fit-content",
                           borderRadius: 30,
                         }}
                       >
-                        
-                        <SearchIcon/>
-
+                        <SearchIcon />
                       </Button>
                     </InputAdornment>
                   ),
