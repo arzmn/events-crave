fcf5b29bb339ab2a9547fc7528af2b0b69c5c416
Chinmay Singh <134195641+Chinmay67@users.noreply.github.com>
2024-09-26 20:29:57 +0530

    "Added Firebase configuration and initialization, dotenv setup, and MongoDB connection to server/index.js"

--- server/index.js
+++ server/index.js
@@ -1,1 +1,43 @@
 // server
+
+import dotenv from 'dotenv';
+import connectDB from './src/db/ConnectDB.js';
+import { app } from './app.js';
+
+dotenv.config({
+    path:'./.env'
+});
+
+// console.log(process.env.PRINT)
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed ",err);
+})
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAi9G_8ursdcOcvRxDrP-W9XDN_Um0N3VU",
+  authDomain: "event-crave.firebaseapp.com",
+  projectId: "event-crave",
+  storageBucket: "event-crave.appspot.com",
+  messagingSenderId: "411075675475",
+  appId: "1:411075675475:web:627d348cdc9656dc780da6",
+  measurementId: "G-WGQM8P0C7M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ server/src/db/ConnectDB.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+const connectDB=async()=>{
+    try {
+        const connectionInstance=await mongoose.connect
+        (`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        console.log(`\n MONGODB CONNECTION SUCCESFULL`,
+        connectionInstance.connection.host);
+        
+    } catch (error) {
+        console.log("MONGODB connection error",error)
+        process.exit(1)
+    }
+}
+
+ export default connectDB
