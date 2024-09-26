a57b4e74a83bdbc25c06b2b1a24985f0c57f2838
Chinmay Singh <134195641+Chinmay67@users.noreply.github.com>
2024-09-26 20:41:44 +0530

    "Remove Firebase configuration and initialization code"

--- server/index.js
+++ server/index.js
@@ -20,24 +20,3 @@ connectDB()
     console.log("Mongodb connection failed ",err);
 })
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAi9G_8ursdcOcvRxDrP-W9XDN_Um0N3VU",
-  authDomain: "event-crave.firebaseapp.com",
-  projectId: "event-crave",
-  storageBucket: "event-crave.appspot.com",
-  messagingSenderId: "411075675475",
-  appId: "1:411075675475:web:627d348cdc9656dc780da6",
-  measurementId: "G-WGQM8P0C7M"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
